708f5bcca4e4f9855eaf46460b7984443e63b488
Yasna Thasneem <yasnathasneem@gmail.com>
2026-06-16 00:28:37 +0530

    Connected frontend and backend

--- backend/app/main.py
+++ backend/app/main.py
@@ -7,3 +7,22 @@ def root():
     return{
         "message" : "NewsTalk AI Backend Running..."
            }
+@app.get("/news")
+def news():
+   return [
+    {
+        "id": 1,
+        "title": "OpenAI launches new AI model",
+        "category": "Technology"
+    },
+    {
+        "id": 2,
+        "title": "India wins cricket series",
+        "category": "Sports"
+    },
+    {
+        "id": 3,
+        "title": "New space mission announced",
+        "category": "Science"
+    }
+]
